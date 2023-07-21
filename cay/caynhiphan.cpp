800637a6dd820d8673cf1fad2c1979a137ff7e0a
Hizu13 <130536281+Hizu13@users.noreply.github.com>
2023-07-21 15:45:55 +0700

    caynhiphan

---
+++ cay/caynhiphan.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+//Khai bao!!
+struct Caynhiphan{
+    int elem;
+    Caynhiphan* phai;
+    Caynhiphan* trai;
+};
+
+struct Sinhvien{
+	int tuoi;
+	
+};
+
+//khoi tao
+void bstInit( Timkiemnhiphan & tree){
+    tree.root = null; //ban dau cay rong
+}
+//ham xoa rong
+void btsMakeEmpty(Caynhiphan * & t){
+    if( t = null) return;
+    btsMakeEmpty(t->)
+}
+
+int main() {
+	Caynhiphan* t;
+	t->phai
+	
+	Sinhvien* hieu;
+	hieu->tuoi
+
+}
